78eb5159600a5c075323cba29970ad78762c5b0f
mohammed-ben-cheikh <bencheikh.official@gmail.com>
2025-11-27 16:40:39 +0100

    chore: Update framework to version 2.3.4 and relocate arcanajs.config.ts template to project root.

--- src/cli/templates.ts
+++ src/cli/templates.ts
@@ -1,9 +1,9 @@
 export const configFiles = [
   { src: "package.json", dest: "package.json" },
   { src: "tsconfig.json", dest: "tsconfig.json" },
-  { src: "arcanajs.d.ts", dest: "src/arcanajs.d.ts" },
-  { src: "arcanajs.config.ts", dest: "src/arcanajs.config.ts" },
+  { src: "arcanajs.config.ts", dest: "arcanajs.config.ts" },
   { src: "postcss.config.js", dest: "postcss.config.js" },
+  { src: "arcanajs.d.ts", dest: "src/arcanajs.d.ts" },
   { src: "globals.css", dest: "src/client/globals.css" },
   { src: "client-index.tsx", dest: "src/client/index.tsx" },
   { src: "server-index.ts", dest: "src/server/index.ts" },
